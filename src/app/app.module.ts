4f714e4faabe309a9312cf5a7791b417a65a8e47
Masaaki Narita <narita.m@gmail.com>
2020-12-25 22:48:22 +0900

    kanseikei

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from "./app.component";
 import { ProductListComponent } from "./product-list/product-list.component";
 import { RouterModule } from "@angular/router";
 import { TopBarComponent } from "./top-bar/top-bar.component";
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, TopBarComponent],
+  declarations: [AppComponent, ProductListComponent, TopBarComponent, ProductAlertsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
