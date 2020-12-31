ba807ab057242ef715f5abf51c6200f987a7a7ae
Masaaki Narita <narita.m@gmail.com>
2020-12-31 13:22:27 +0900

    2 commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,24 @@ import { AppComponent } from "./app.component";
 import { ProductListComponent } from "./product-list/product-list.component";
 import { RouterModule } from "@angular/router";
 import { TopBarComponent } from "./top-bar/top-bar.component";
-import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductAlertsComponent } from "./product-alerts/product-alerts.component";
+import { ProductDetailsComponent } from "./product-details/product-details.component";
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, TopBarComponent, ProductAlertsComponent],
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    TopBarComponent,
+    ProductAlertsComponent,
+    ProductDetailsComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot([{ path: "", component: ProductListComponent }]),
+    RouterModule.forRoot([
+      { path: "", component: ProductListComponent },
+      { path: "products/:productId", component: ProductDetailsComponent },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent],
